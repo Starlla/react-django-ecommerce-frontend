b2652745c94b40b858f95151d4c1181dd75d545b
Claire <claire.cy.t@gmail.com>
2021-11-07 10:35:02 -0500

    clean nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
 
 function Header() {
   return (
-    <div>
-      <header>Header</header>
-    </div>
-  )  
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">ProShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/cart">Cart</Nav.Link>
+              <Nav.Link href="/login">Login</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
