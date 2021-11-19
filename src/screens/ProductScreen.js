41613ea2de45cf2dc8a10e7361b6063d86d8fa50
Claire <claire.cy.t@gmail.com>
2021-11-19 11:18:54 -0500

    get product id

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { Link, useParams } from "react-router-dom";
+import { Row, Col, Image, ListGroup, Button, Card } from "react-bootstrap";
+import Rating from "../components/Rating";
+import products from "../products";
 
 function ProductScreen() {
-  return <div>Product</div>;
+  let params = useParams();
+  const product = products.find((p) => p._id === params.id);
+  return <div>{product.name}</div>;
 }
 
 export default ProductScreen;
