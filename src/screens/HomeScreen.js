58a67d59771ce9e59ccbaae6ee9276425792332b
Claire <claire.cy.t@gmail.com>
2022-02-03 15:50:01 -0500

    use proxy

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -9,8 +9,8 @@ function HomeScreen() {
 
   useEffect(() => {
     async function fetchProducts() {
-      const { data } = await axios.get("http://127.0.0.1:8000/api/products/");
-      console.log(data);
+      const { data } = await axios.get("/api/products/");
+      setProducts(data);
     }
     fetchProducts();
   }, []);
